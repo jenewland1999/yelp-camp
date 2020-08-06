a323a5fe3a920c3ac312ea73fa4b8eda5599b16f
jenewland1999 <github@jenewland.me.uk>
2020-08-06 10:03:38 +0100

    ✨ Complete stage v04

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ seedDB();
 
 // Models
 const Campground = require("./models/campground");
+const Comment = require("./models/comment");
 
 mongoose.connect("mongodb://localhost/yelpcamp", {
   useNewUrlParser: true,
@@ -28,7 +29,10 @@ app.get("/campgrounds", (req, res) => {
     if (err) {
       console.error(err);
     } else {
-      res.render("index", { title: "Campgrounds", campgrounds: campgrounds });
+      res.render("campgrounds/index", {
+        title: "Campgrounds",
+        campgrounds: campgrounds,
+      });
     }
   });
 });
@@ -52,7 +56,7 @@ app.post("/campgrounds", (req, res) => {
 });
 
 app.get("/campgrounds/new", (req, res) => {
-  res.render("new", { title: "Campgrounds - New" });
+  res.render("campgrounds/new", { title: "Campgrounds - New" });
 });
 
 app.get("/campgrounds/:id", (req, res) => {
@@ -63,12 +67,44 @@ app.get("/campgrounds/:id", (req, res) => {
         console.error(err);
       } else {
         console.log(campground);
-        res.render("show", {
+        res.render("campgrounds/show", {
           title: "Campgrounds - Show",
           campground: campground,
         });
       }
     });
+});
+
+app.get("/campgrounds/:id/comments/new", (req, res) => {
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.render("comments/new", { title: "", campground: campground });
+    }
+  });
+});
+
+app.post("/campgrounds/:id/comments", (req, res) => {
+  // Retrieve the campground
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.error(err);
+      res.redirect("/campgrounds");
+    } else {
+      // Create new comment
+      Comment.create(req.body.comment, (err, comment) => {
+        if (err) {
+          console.error(err);
+        } else {
+          campground.comments.push(comment._id);
+          campground.save();
+          res.redirect(`/campgrounds/${campground._id}`);
+        }
+      });
+    }
+  });
+  // Interlink the comment and campground
 });
 
 app.listen(3000, () => {
